7e75b4abf0ecef8b71628cdeaabf790bee3cdf55
buko106 <buko1062000@yahoo.co.jp>
2017-06-30 15:33:08 +0900

    create very simple dataset

---
+++ main.py
@@ -0,0 +1,82 @@
+import os
+import argparse
+import numpy as np
+
+# argparse configurations
+desc="Script for generating train/test dataset from MovieLens100k(https://grouplens.org/datasets/movielens/)"
+parser = argparse.ArgumentParser(description=desc)
+parser.add_argument("--prefix-train", type = str, default="train_", help="default='train_'")
+parser.add_argument("--prefix-test" , type = str, default="test_" , help="default='test_'")
+parser.add_argument("--dataset"     , metavar="DATA",type = str, default="dataset/",help="default='dataset/'" )
+parser.add_argument("-o", "--output", metavar="DIR" ,type = str, default="output" , help="default='output/'")
+parser.add_argument("-t", "--timestamp", action="store_true")
+parser.add_argument("-g", "--genre", action="store_true")
+parser.add_argument("-a", "--age", action="store_true")
+parser.add_argument("-s", "--sex", action="store_true")
+parser.add_argument("-j", "--job", action="store_true")
+parser.add_argument("--other", action="store_true")
+parser.add_argument("--last", action="store_true")
+args = parser.parse_args()
+
+# not implemented
+if args.other :
+    print("[ERROR] --other is not implemented")
+    exit(1)
+    
+if args.last :
+    print("[ERROR] --last is not implemented")
+    exit(1)
+
+# read args
+output  = args.output
+dataset = args.dataset
+itemfile  = os.path.join(dataset,"u.item.utf8")
+userfile  = os.path.join(dataset,"u.user")
+occupationfile = os.path.join(dataset,"u.occupation")
+
+# create output directory
+if not os.path.exists(output):
+    os.makedirs(output)
+    
+
+# input data description
+name = [ "u1", "u2", "u3", "u4", "u5" ]
+ext_train = ".base"
+ext_test  = ".test"
+
+# read item file
+movie_to_genre_vector = {}
+for line in open(itemfile,"r"):
+    line = line.rstrip("\n").split("|")
+    movie = int(line[0])
+    movie_to_genre_vector[movie] = np.array(line[5:],dtype=int)
+# read occupation file
+occupation_to_number = { occ:i for (i,occ) in enumerate(open(occupationfile,"r").read().split()) }
+# read user file
+id_to_userinfo = {} # ( age, is_female, occupation_number )
+for line in open(userfile,"r"):
+    line = line.rstrip("\n").split("|")
+    id = int(line[0])
+    id_to_userinfo[id] = (int(line[1]),line[2]=="F",occupation_to_number[line[3]])
+
+def convert_to_libfm( infile, outfile ):
+    with open(outfile,"w") as out:
+        for line in open(infile):
+            offset = 0
+            line = line.rstrip("\n").split("\t")
+            id, movie, rate, time = line
+            # rate
+            out.write(rate)
+            # user id
+            out.write(" "+str(offset+int(id))+":1")
+            offset += len(id_to_userinfo)
+            # movie id
+            out.write(" "+str(offset+int(movie))+":1")
+            offset += len(movie_to_genre_vector)
+            
+            out.write("\n")
+
+# create libfm dataset
+for u in name:
+    convert_to_libfm( os.path.join(dataset,u+ext_train), os.path.join(output,args.prefix_train+u+".txt"))
+    convert_to_libfm( os.path.join(dataset,u+ext_test ), os.path.join(output,args.prefix_test +u+".txt"))
